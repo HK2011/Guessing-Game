214b2bd06b880494a5744c3f2fc7ff52322facb1
HK2011 <mkcreation1981@gmail.com>
2025-01-18 13:07:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let randomNumber = (min, max) =>
 let guessingNumber = randomNumber(1, 100);
 let submit = document.querySelector(".submit");
 console.log(guessingNumber);
-document.querySelector(".guess_number").textContent = guessingNumber;
+document.querySelector(".guess").textContent = "?";
+document.querySelector(".guess").textContent = guessingValue;
 
-document.querySelector(".guess_number").style.display = "none";
 let score = 100;
 
 function guessedAnswer() {
@@ -14,13 +14,13 @@ function guessedAnswer() {
   console.log(guessingValue);
 
   if (guessingValue < guessingNumber) {
-    document.getElementById("result").textContent = "Too Low😦";
+    document.getElementById("result").textContent = "Too Low😔";
     score = score - 10;
-    document.getElementById("score").textContent = score;
+    document.querySelector(".score").textContent = score;
   } else if (guessingValue > guessingNumber) {
     document.getElementById("result").textContent = "Too High🤔";
     score = score - 10;
-    document.getElementById("score").textContent = score;
+    document.querySelector(".score").textContent = score;
   } else {
     document.getElementById("result").textContent = "You Won😊";
     document.querySelector(".guess_number").style.display = "block";
@@ -29,20 +29,13 @@ function guessedAnswer() {
     document.getElementById("result").textContent =
       "you lost .Click restart to keep playing";
   }
-  // if (score == 0) {
-  //   submit.style.display = "none";
-  //   document.getElementById("score").textContent = "you lost";
-  // }
+
   if (score == -10) {
     location.reload();
   }
+  console.log(score);
 }
 function restart() {
-  guessingNumber = randomNumber(1, 100);
-  score = 100;
-  document.getElementById("score").textContent = score;
-  document.getElementById("result").textContent = "Result";
-  document.querySelector(".guess_number").style.display = "none";
-  document.getElementById("guessing_value").value = "";
-  console.log(guessingNumber);
+  location.reload();
 }
+
